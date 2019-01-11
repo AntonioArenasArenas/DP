32a874a632c661d18fee5ed0422caaa63aadff65
Antonio Arenas Arenas <antonioarenasarenas92@gmail.com>
2019-01-11 11:54:14 +0100

    Merge de Admin y Administrator
    Se ha quedado la clase final como Admin

--- Acme-Handy-Worker/src/main/java/controllers/AdminController.java
+++ Acme-Handy-Worker/src/main/java/controllers/AdminController.java
@@ -15,100 +15,141 @@ import security.UserAccount;
 import security.UserAccountService;
 import services.ActorService;
 import services.AdminService;
+import services.ApplicationService;
+import services.TaskService;
 import domain.Admin;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController extends AbstractController {
 
-	
 	@Autowired
 	private AdminService adminService;
 	@Autowired
 	private UserAccountService userAccountService;
 	@Autowired
 	private ActorService actorService;
+	@Autowired
+	private ApplicationService applicationService;
 
-	
 	// Constructors -----------------------------------------------------------
 
 	public AdminController() {
 		super();
 	}
-	
+
+	// Action-1 ---------------------------------------------------------------
+
+	@RequestMapping(value = "/statistics", method = RequestMethod.GET)
+	public ModelAndView statistics() {
+		ModelAndView result;
+
+		// Double[] spuser = taskService.tasksPerUserStatistics();
+
+		Double[] sptask = applicationService.getAdminStatisticsPerTask();
+
+		Double[] spoffered = applicationService
+				.getAdminStatisticsPriceOffered();
+
+		Double cantChange = applicationService.getAppliCantChange();
+
+		result = new ModelAndView("admin/statistics");
+
+		// result.addObject("maximumpu", spuser[2]);
+		// result.addObject("minimumpu", spuser[1]);
+		// result.addObject("averagepu", spuser[0]);
+		// result.addObject("stdevpu", spuser[3]);
+
+		result.addObject("maximumpt", sptask[2]);
+		result.addObject("minimumpt", sptask[1]);
+		result.addObject("averagept", sptask[0]);
+		result.addObject("stdevpt", sptask[3]);
+
+		result.addObject("maximumpo", spoffered[2]);
+		result.addObject("minimumpo", spoffered[1]);
+		result.addObject("averagepo", spoffered[0]);
+		result.addObject("stdevpo", spoffered[3]);
+
+		result.addObject("ratio", cantChange);
+
+		return result;
+	}
+
 	// Edit
-	
+
 	@RequestMapping(value = "/create", method = RequestMethod.GET)
 	public ModelAndView create() {
 		ModelAndView result;
 		Admin admin;
 
-		admin = this.adminService.create() ;
+		admin = this.adminService.create();
 		result = this.createEditModelAndView(admin);
 
 		return result;
 	}
-	
+
 	@RequestMapping(value = "/edit", method = RequestMethod.GET)
 	public ModelAndView edit() {
 		ModelAndView result;
-	
+
 		Admin admin = adminService.findByPrincipal();
 		Assert.notNull(admin);
 		result = this.createEditModelAndView(admin);
 
 		return result;
 	}
-	
-	
+
 	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
-	public ModelAndView save(@Valid final Admin admin, final BindingResult binding) {
+	public ModelAndView save(@Valid final Admin admin,
+			final BindingResult binding) {
 		ModelAndView result;
 
-		if (binding.hasErrors()){
-			
+		if (binding.hasErrors()) {
+
 			result = this.createEditModelAndView(admin);
 			System.out.println(binding.getAllErrors());
-			
-			}
-		else
-			try {	
-				
+
+		} else
+			try {
+
 				UserAccount userAccount = admin.getUserAccount();
-				final String password = new Md5PasswordEncoder().encodePassword(userAccount.getPassword(), null);
+				final String password = new Md5PasswordEncoder()
+						.encodePassword(userAccount.getPassword(), null);
 				userAccount.setPassword(password);
 				userAccount = this.userAccountService.save(userAccount);
 				admin.setUserAccount(userAccount);
-				
+
 				this.adminService.save(admin);
 				result = new ModelAndView("redirect:/");
-				
+
 			} catch (final Throwable oops) {
-				result = this.createEditModelAndView(admin, "admin.commit.error");
-		}
+				result = this.createEditModelAndView(admin,
+						"admin.commit.error");
+			}
 
 		return result;
 	}
 
 	// Ancillary methods ------------------------------------------------------
 
-				protected ModelAndView createEditModelAndView(final Admin admin) {
-					ModelAndView result;
+	protected ModelAndView createEditModelAndView(final Admin admin) {
+		ModelAndView result;
 
-					result = this.createEditModelAndView(admin, null);
+		result = this.createEditModelAndView(admin, null);
 
-					return result;
-				}
+		return result;
+	}
 
-				protected ModelAndView createEditModelAndView(final Admin admin, final String messageError) {
-					ModelAndView result;
+	protected ModelAndView createEditModelAndView(final Admin admin,
+			final String messageError) {
+		ModelAndView result;
 
-					result = new ModelAndView("admin/edit");
-					result = new ModelAndView("admin/create");
-					result.addObject("admin", admin);
-					result.addObject("message", messageError);
+		result = new ModelAndView("admin/edit");
+		result = new ModelAndView("admin/create");
+		result.addObject("admin", admin);
+		result.addObject("message", messageError);
 
-					return result;
-				}
+		return result;
+	}
 
 }
